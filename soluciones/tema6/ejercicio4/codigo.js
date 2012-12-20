2dfd60f424b141b407368e8527542b55d8425c0d
Elías Alonso <eliasagc@gmail.com>
2012-12-20 21:25:17 +0100

    el visor de fotos, primera version

---
+++ soluciones/tema6/ejercicio4/codigo.js
@@ -0,0 +1,47 @@
+$(function() {
+  
+  // La foto que estoy viendo
+  var posicion = 0;
+  
+  // Cuántas fotos hay
+  
+  var numeroFotos = $(".orbit img").length;
+  
+  // Los nodos que me interesan
+  
+  var botones = $("#botones a");
+  var fotos = $("#featured");
+
+  // Para viajar a la posición que quiero ver
+  
+  function moverAPosicion () {
+    posicion = posicion % numeroFotos;
+    fotos.stop().animate({left: -100 * posicion + "%"});
+  }
+
+  // Botones [<] y [>] del visor
+  
+  $("#izquierda").click(function (e) {
+    posicion = posicion - 1;
+    moverAPosicion();
+  });
+
+  $("#derecha").click(function (e) {
+    posicion = posicion + 1;
+    moverAPosicion();
+  });
+  
+  // Botones [ 1 | 2 | 3 ]
+  
+  function botonIrAFoto (boton, n) {
+    $(boton).click(function () {
+      posicion = n;
+      moverAPosicion();
+    })
+  }
+
+  for (var i=0; i<botones.length; i++) {
+    botonIrAFoto(botones[i], i);
+  }
+
+});
--- tema6/ejercicio5/codigo.js
+++ tema6/ejercicio5/codigo.js
@@ -1,23 +1,9 @@
 $(function() {
 
-  // Cambia este valor (slow, fast, o el número de milisegundos)
+  var botones = $(".mostrar");
+  var contenidos = $(".panel");
 
-  var velocidad = "slow";
-
-  // No te preocupes por esto...
-
-  function mostrarYOcultar (selector, entrar, salir) {
-    $(selector + " .mostrar").click(function() {
-      $(selector + " .panel")[entrar](velocidad);
-    });
-
-    $(selector + " .ocultar").click(function() {
-      $(selector + " .panel")[salir](velocidad);
-    });
-  }
-
-  mostrarYOcultar(".normal", "show", "hide");
-  mostrarYOcultar(".fade", "fadeIn", "fadeOut");
-  mostrarYOcultar(".slide", "slideDown", "slideUp");
-
+  $(botones[0]).click(function (e) {
+    $(contenidos[0]).hide('slow').fadeOut();
+  });
 });
