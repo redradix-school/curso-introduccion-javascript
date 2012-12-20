1135822b8e48ba36d3d2ed98d7267a26477f97aa
Elías Alonso <eliasagc@gmail.com>
2012-12-20 16:12:32 +0100

    wrap

--- tema5/ejercicio2/ejercicio2.js
+++ tema5/ejercicio2/ejercicio2.js
@@ -1,9 +1,12 @@
-function ponEventos (linea) {
-  // Recibe: un nodo div.linea
-  // Se encarga de poner los eventos adecuados
-  // a sus botones
-}
+$(function () {
+  function ponEventos (linea) {
+    // Recibe: un nodo div.linea
+    // Se encarga de poner los eventos adecuados
+    // a sus botones
+  }
 
-var lineas = $(".linea");
+  var lineas = $(".linea");
 
-// ???
+  // ???
+  
+});
