43da36d99507755a7b1ee42b1dfff65e380bea26
Elías Alonso <eliasagc@gmail.com>
2013-03-04 19:09:58 +0100

    starting a new edition

--- soluciones/tema4/esta_en_la_lista.js
+++
@@ -1,8 +0,0 @@
-function estaEnLaLista (lista, elemento) {
-  for (var i=0; i<lista.length; i++) {
-    if (elemento == lista[i]) {
-      return true;
-    }
-  }
-  return false;
-}
--- soluciones/tema6/ejercicio2/codigo.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-  $("#boton").click(function (e) {
-    $("#fondo, #modal").addClass("visible");
-  });
-  $("#modal > a, #fondo").click(function (e) {
-    $("#fondo, #modal").removeClass("visible");
-  })
-});
